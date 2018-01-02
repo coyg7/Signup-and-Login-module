be9fb36dac6195fa9a15cee1d5c0ea0804d83d36
coyg7 <ssachit3761@gmail.com>
2018-01-02 12:14:52 +0530

    Generate token on successful login

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,8 @@
 import express from "express";
 import User from "../models/user";
 import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken';
+import config from '../config';
 
 
 let router = express.Router();
@@ -16,7 +18,11 @@ router.post("/", (req, res) => {
     .then(user => {
       if (user) {
         if (bcrypt.compareSync(password, user.get("password_digest"))) {
-
+          const token = jwt.sign({
+            id: user.get('id'),
+            username: user.get('username')
+          }, config.jwtSecret);
+          res.json({ token });  
         } else {
           res.status(401).json({ errors: { form: "Invalid Credentials" } });
         }
